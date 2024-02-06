4b4cfd4bb895322fd7cb83ac9df85145f74fc7ac
sangam <sangam@blueaves.com>
2024-02-06 10:56:39 +0530

    auth sevrice is added

--- src/services/createuserservice.ts
+++ src/services/createuserservice.ts
@@ -1,18 +1,31 @@
+import User from '../interfaces/IUser';
 import userModel from '../model/userModel';
+import bcrypt from "bcrypt"
+import { userResponse } from '../response/userResponse';
 class createUserService{
-     async createUser(userdata: {username:string, email : string}){
-        const {username, email} = userdata;
+    public static transformUserResponse(user: User): userResponse {
+        return new userResponse(user.username, user.email);
+    }
+     async createUser(userdata: User){
+        const {username, email , password} = userdata;
         const  existingUser = await userModel.findOne({ $or:[{username},{email}]});
         if (existingUser) {
             throw new Error('user already exist');
         }
-        const newUser = new userModel({ username, email });
+        let hashedPassword ;
+        try{
+            hashedPassword = await bcrypt.hash(password,10);
+        }
+        catch (error) {
+            console.error(error)
+            throw new Error('Failed to hash password')
+        }
+        const newUser = new userModel({ username, email , password: hashedPassword });
         await newUser.save();
         return newUser;
     }
 }
 export default createUserService;
-
 //const createuser = async (req: Request, res: Response) => {
 
     //try {
